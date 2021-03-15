edb758cf5caf1f5a061efb7842362d0fa02ef50d
Alyona Haiova <alenag2602@gmail.com>
2021-03-15 12:04:20 +0200

    Add Model

---
+++ file.js
@@ -0,0 +1,12 @@
+class File{
+    id;
+    name;
+    path;
+    size;
+    constructor(id, name, path, size){
+        this.id = id;
+        this.name = name;
+        this.path = path;
+        this.size = size;
+    }
+}
---
+++ rating.js
@@ -0,0 +1,11 @@
+class Rating{
+    id;
+    idUser;
+    idFile;
+    constructor(id, idUser, idFile) {
+        this.id = id;
+        this.idUser = idUser;
+        this.idFile = idFile;
+    }
+
+}
